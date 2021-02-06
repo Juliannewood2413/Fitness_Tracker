9a4a9a4eb3762277d983607286771b7435ab9898
Jules <juliannewood2413@gmail.com>
2021-02-06 16:25:27 -0700

    updated routes for creating/updating workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,54 +7,65 @@ module.exports = function(app) {
 //get last workout
 app.get('/api/workouts', (req,res) => {
     db.Workout.find({})
-    .then(workout => {
-        res.json(workout);
+    .then(dbWorkout => {
+        res.json(dbWorkout);
     })
     .catch(err => {
-        res.json(err);
+        res.status(400).json(err);
     })
 });
 
 //create new workout
-app.post('/api/workouts', async (req,res) => {
-  try{
-      const input = await db.Workout.create({ type: 'workout'})
-      res.json(input);
-  }
-  catch(err){
-      console.log("Couldn't add workout at this time", err)
-  }
+app.post('/api/workouts', (req,res) => {
+  db.Workout.create(req.body)
+  .then((dbWorkout) => {
+      res.json(dbWorkout);
+  }).catch(err => {
+      res.status(400).json(err)
+  })
 });
 
 //update/add exercise type
-app.put("/api/workouts/:id", ({body, params}, res) => {
-    const exerciseId = params.id;
-    let addedExercises = [];
-
-    db.Workout.find({_id: exerciseId})
-    .then(dbWorkout => {
-        addedExercises = dbWorkout[0].exercises;
-        res.json(dbWorkout[0].exercises);
-        let allExercises = [...savedExercises, body];
-        updateWorkout(allExercises);
+app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+      { _id: req.params.id }, { exercises: req.body }
+    ).then((dbWorkout) => {
+      res.json(dbWorkout);
+    }).catch(err => {
+      res.status(400).json(err);
     });
-
-    function updateWorkout(exercises){
-        db.Workout.findByIdAndUpdate(exerciseId, {exercises: exercises}, function(err, doc) {
-            if(err){
-                console.log(err)
-            }
-        });
-    }
 });
 
-app.get('/api/workouts/range', (req, res) => {
+
+// app.put("/api/workouts/:id", ({body, params}, res) => { 
+//     console.log(body, params)
+//     const exerciseId = params.id;
+//     let addedExercises = [];
+
+//     db.Workout.findById({_id: exerciseId})
+//     .then(dbWorkout => {
+//         addedExercises = dbWorkout[0].exercises;
+//         res.json(dbWorkout[0].exercises);
+//         let allExercises = [...addedExercises, body];
+//         updateWorkout(allExercises);
+//     });
+
+//     function updateWorkout(exercises){
+//         db.Workout.findByIdAndUpdate(exerciseId, {exercises: exercises}, function(err, doc) {
+//             if(err){
+//                 console.log(err)
+//             }
+//         });
+//     }
+// });
+
+app.get('/api/workouts/range', ({}, res) => {
     db.Workout.find({})
-    .then(workout => {
-        res.json(workout);
+    .then(dbWorkout => {
+        res.json(dbWorkout);
     })
     .catch(err =>{
-        res.json(err);
+        res.status(400).json(err);
     })
 })
 }
